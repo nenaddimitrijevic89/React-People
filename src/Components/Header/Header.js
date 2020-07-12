b9bacc4b92053d36a0042ea6d66e1b78ca0b0f4a
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-07-12 23:19:16 +0200

    header fixing

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,30 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Header.css';
+import style from './Header.module.css';
 import { Navbar, Icon, NavItem } from 'react-materialize';
 
 
 const Header = ({ isList, onLayoutChange, updateUsers }) => {
     return (
-        // <Container fluid className="header">
-        //     <Container>
-        //         <Row>
-        //             <Col lg={{ span: 2, offset: 5 }} md={{ span: 6, offset: 3 }} sm={{ span: 4, offset: 3 }}>
-        //                 <h2 className="header__title">Bit People</h2>
-        //             </Col>
-        //             <Col className="header__icons" lg={{ span: 2, offset: 3 }} md={{ span: 1, offset: 2 }} sm={{ span: 1, offset: 4 }}>
-        //                 <i className="fa fa-refresh header__icon" onClick={updateUsers}></i>
-        //                 {isList
-        //                     ? <i className="fa fa-th-large header__icon" onClick={onLayoutChange}></i>
-        //                     : <i className="fa fa-list header__icon" onClick={onLayoutChange}></i>
-        //                 }
-        //             </Col>
-        //         </Row>
-        //     </Container>
-        // </Container>
-
-
         <Navbar
+            className={style.header}
             alignLinks="right"
             brand={<a className="brand-logo">React People</a>}
             centerChildren
