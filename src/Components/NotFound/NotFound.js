c80707f22282d2547c688a13a1ae9f9ac50fbdd9
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-07-13 21:25:15 +0200

    about page first part done

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import notFound from '../../Images/not_found.png';
 import { Container, Row, Col } from 'react-bootstrap';
+import style from './NotFound.module.css';
 
 
 const NotFound = () => {
     return (
-        <Container>
+        <Container className={style.sticky}>
             <Row>
                 <Col>
                     <img src={notFound}></img>
