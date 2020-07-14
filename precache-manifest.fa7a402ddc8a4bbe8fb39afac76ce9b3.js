5ff3ea3760092f2402cf7cf88d83591f03a0a2e7
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-07-14 21:51:14 +0200

    Updates

---
+++ precache-manifest.fa7a402ddc8a4bbe8fb39afac76ce9b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5925724ac8b64a3412675b9d7a564054",
+    "url": "/react-bit-people/index.html"
+  },
+  {
+    "revision": "9a5a9d254c69605b43bd",
+    "url": "/react-bit-people/static/css/2.0732034e.chunk.css"
+  },
+  {
+    "revision": "36cf82a35f30cd988ea3",
+    "url": "/react-bit-people/static/css/main.e51b44a9.chunk.css"
+  },
+  {
+    "revision": "9a5a9d254c69605b43bd",
+    "url": "/react-bit-people/static/js/2.adc3cf49.chunk.js"
+  },
+  {
+    "revision": "8debe1713c30c1c8fa79b57451984727",
+    "url": "/react-bit-people/static/js/2.adc3cf49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36cf82a35f30cd988ea3",
+    "url": "/react-bit-people/static/js/main.ae0863a4.chunk.js"
+  },
+  {
+    "revision": "c908fa419fcf8bcb7502",
+    "url": "/react-bit-people/static/js/runtime-main.43a928fc.js"
+  },
+  {
+    "revision": "74262339fa4cc98af5157734e0637719",
+    "url": "/react-bit-people/static/media/not_found.74262339.png"
+  }
+]);
