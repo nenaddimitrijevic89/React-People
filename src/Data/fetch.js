3014640424a9aa2f14b848fcfb662b0059109216
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-07-14 21:49:52 +0200

    deploy react people to github

--- src/Data/fetch.js
+++ src/Data/fetch.js
@@ -2,6 +2,7 @@ const fetchUsers = () => {
     return (
         fetch('https://randomuser.me/api/?results=15')
             .then(response => response.json())
+            .catch(error => console.log(error))
     )
 }
 
