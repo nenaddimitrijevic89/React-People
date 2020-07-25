76d42aab0c84654e21eda3799214f8761dc5f241
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-07-25 21:42:05 +0200

    Updates

---
+++ precache-manifest.90da3ab0f09f2ffb42ee7713f984f08b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "735b9ce4513d68acb9b972609058eecc",
+    "url": "/React-People/index.html"
+  },
+  {
+    "revision": "4d6b2fa7bd59cae085cc",
+    "url": "/React-People/static/css/2.0732034e.chunk.css"
+  },
+  {
+    "revision": "84a4baa33db0e5434cbc",
+    "url": "/React-People/static/css/main.b284b83c.chunk.css"
+  },
+  {
+    "revision": "4d6b2fa7bd59cae085cc",
+    "url": "/React-People/static/js/2.f12eebd0.chunk.js"
+  },
+  {
+    "revision": "8debe1713c30c1c8fa79b57451984727",
+    "url": "/React-People/static/js/2.f12eebd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a4baa33db0e5434cbc",
+    "url": "/React-People/static/js/main.0ee98159.chunk.js"
+  },
+  {
+    "revision": "396aca85fa36fa57c7b3",
+    "url": "/React-People/static/js/runtime-main.eb1a16c0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-People/precache-manifest.cb7d3847e9843cd1e8f0015a6a187904.js"
+  "/React-People/precache-manifest.90da3ab0f09f2ffb42ee7713f984f08b.js"
 );
 
 self.addEventListener('message', (event) => {
